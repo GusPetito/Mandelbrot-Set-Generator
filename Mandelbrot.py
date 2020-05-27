4d92b62c7f96c13eab4940641a6309cdcf2a2ea1
GusPetito <guspetito@gmail.com>
2020-05-26 19:24:24 -0500

    implemented growth factor text box and info panel ranges

--- Mandelbrot.py
+++ Mandelbrot.py
@@ -3,6 +3,7 @@ import time
 import pygame
 import matplotlib.pyplot as plt
 import math
+from NumberInputBox import NumberInputBox
 
 #The recursive function to be used in generating the set
 def generatingFunc(z, constant):
@@ -14,46 +15,6 @@ def giveTime(start):
     minutes = int(now / 60)
     seconds = round(round(now) % 60)
     return str(minutes) + "' " + str(seconds) + '"'
-
-#Height and length correspond to the pixel dimensions of the graph, which means it's also how many points will be analyzed in a column or row, respectively
-def generatePoints(xMin, xMax, yMin, yMax, length, height, maxIt, growthFactor, screen):
-    #img = Image.new('RGB', (length+1, height+1), color=0)
-    start = time.time()
-
-    y = yMin
-    yCounter = 0
-    while(y < yMax):
-        print(str(round(100*(y-yMin)/(yMax-yMin),2)) + " percent done (y = " + str(round(y,2)) + ")")
-        print("Time: " + giveTime(start))
-
-        xCounter = 0
-        x = xMin
-        while(x < xMax):
-            count = 0
-            tempVal = 0
-            testedVal = complex(x,y)
-
-            #while(it's been too long or it's escaped the bounds)
-            while(count < maxIt and abs(tempVal) < 2):
-                tempVal = generatingFunc(tempVal, testedVal)
-                count += 1
-            color = int(pow(count,growthFactor) * 255 / pow(maxIt,growthFactor))
-            if(count == maxIt): 
-                #img.putpixel((xCounter,yCounter), (0,0,0))
-                screen.set_at((xCounter, yCounter), (0,0,0))
-            else:
-                #img.putpixel((xCounter,yCounter), (int(color/1.3),color,int(color/1.3)))
-                screen.set_at((xCounter, yCounter), (int(color/1.3),color,int(color/1.3)))
-            x += (xMax-xMin) / (length)
-            xCounter += 1
-
-        y += (yMax-yMin) / (height)
-        yCounter += 1
-
-        pygame.display.flip()
-
-    #img.show()
-    #img.save("mandelbrot3.png","PNG")
 
 #Takes 3 2d arrays of color values and combines them
 def combineColors(r, g, b):
@@ -73,16 +34,18 @@ def toRGB(vals):
     g = 255 - b - r
     return combineColors(r, g, b)
 
-def toGreen(vals):
+#Returns a 3d array for a heat map using the given RGB and values, with black values given to the maximum values
+def toColor(vals, color):
     maximum = np.amax(vals)
     ratio = vals / maximum
     ratio[ratio == 1] = 0
-    g = ratio * 255
-    r = g / 1.3
-    b = g / 1.3
+    r, g, b = color
+    r *= ratio
+    g *= ratio
+    b *= ratio
     return combineColors(r, g, b)
 
-def generatePointsNumpy(xRange, yRange, length, height, maxIt, growthFactor):
+def generatePointsNumpy(xRange, yRange, length, height, maxIt, growthFactor, brot_color):
     x = np.linspace(xRange[0], xRange[1], length)
     y = np.linspace(yRange[0], yRange[1], height)
     isIn = np.full((length, height), True, dtype=bool)
@@ -91,11 +54,11 @@ def generatePointsNumpy(xRange, yRange, length, height, maxIt, growthFactor):
     z = np.copy(c)
     for i in range(maxIt):
         print(str(round(100 * i / maxIt)) + " percent done. Iteration " + str(i))
-        z[isIn] = z[isIn]**2 + c[isIn]
+        z[isIn] = generatingFunc(z[isIn], c[isIn])
         isIn[np.abs(z) > 2] = False
         mandelSet[isIn] = i
-    mandelSet = np.power(mandelSet, growthFactor)
-    mandelSet = toGreen(mandelSet)
+    mandelSet = np.power(mandelSet, 1/growthFactor)
+    mandelSet = toColor(mandelSet, brot_color)
 
     return mandelSet
     #plt.imshow(mandelSet)
@@ -123,6 +86,27 @@ def getRect(initialPos, currentPos, screenRatio, infoPanelHeight):
     tempHeight = currentPos[1] - initialPos[1]
     tempWidth = screenRatio * tempHeight
     return pygame.Rect(initialPos, (tempWidth,tempHeight))
+
+#This will redraw (but not update) the screen
+#Each drawing_list will be a list where drawing_list[0] is the start position, and the rest of the indecies are rows, with each row containing objects to blit
+#NB: There do not need to be the same amount of columns in each row, and they don't have to have constant widths
+def redraw_background(screen, color, *drawing_lists):
+    screen.fill(color)
+    for drawing_list in drawing_lists:
+        current_pos = drawing_list[0]
+        for i in range(1, len(drawing_list)):
+            heights = []
+            for j in drawing_list[i]:
+                if(isinstance(j, NumberInputBox)): #TODO: Make interface
+                    j.top_left = current_pos
+                    temp_surface = j.get_surface()
+                else:
+                    temp_surface = j
+                screen.blit(temp_surface, current_pos)
+                current_pos = (current_pos[0] + temp_surface.get_width(), current_pos[1])
+                heights.append(temp_surface.get_height())
+            max_height = max(heights)
+            current_pos = (drawing_list[0][0], current_pos[1] + max_height)
 
 def main():
     pygame.init()
@@ -155,36 +139,55 @@ def main():
     yRange = (minInitialYRange[0] - (yOffset/2), minInitialYRange[1] + (yOffset/2))
 
     #Aesthetic
-    growthFactor = 1/2
+    growthFactor = 3
     maxIt = 200
     rectColor = (255,255,255)
     default_font = pygame.font.Font(pygame.font.get_default_font(), 15)
     decimal_count = 3
+    brot_color = (255,0,100)
+
+    #Tool objects
+    growth_text = NumberInputBox((0,0), 50, 20, (200,200,200), default_font, str(growthFactor))
+
+    #<---------------------------------------------------------------->
 
     screen = pygame.display.set_mode((screenWidth,screenHeight))
 
     #If a new mandelbrot image needs to be generated
     needToGenerate = False
 
-    brotImage = pygame.surfarray.make_surface(generatePointsNumpy(xRange, yRange, graphWidth, graphHeight, maxIt, growthFactor))
+    brotImage = pygame.surfarray.make_surface(generatePointsNumpy(xRange, yRange, graphWidth, graphHeight, maxIt, growth_text.get_factor(), brot_color))
     screen.blit(brotImage, (0,infoPanelHeight))
     screen.fill((255,255,255))
     #Position of where the mouse was held down, if at all
     mouseInitialPos = None
+
+
     # main loop
+    #<---------------------------------------------------------------->
     running = True
     while running:
 
         if(needToGenerate):
-            brotImage = pygame.surfarray.make_surface(generatePointsNumpy(xRange, yRange, graphWidth, graphHeight, maxIt, growthFactor))
+            brotImage = pygame.surfarray.make_surface(generatePointsNumpy(xRange, yRange, graphWidth, graphHeight, maxIt, growth_text.get_factor(), brot_color))
             needToGenerate = False
 
-        #Refresh background
-        screen.fill((255,255,255))
+        #Calculate and display the current position and range
+        current_range = getPoints(xRange, yRange, pygame.Rect((0,infoPanelHeight),(graphWidth,graphHeight)),graphWidth,graphHeight,infoPanelHeight)
+        current_range_surface = default_font.render(f"Current range: {round(current_range[0][0], decimal_count)} + {round(current_range[1][0], decimal_count)}j to {round(current_range[0][1], decimal_count)} + {round(current_range[1][1], decimal_count)}j", True, (0,0,0))
         current_point = getPoint(xRange, yRange, pygame.mouse.get_pos(), graphWidth, graphHeight, infoPanelHeight)
-        text_surface = default_font.render(f"Current point: {round(current_point[0], decimal_count)} + {round(current_point[1], decimal_count)}j", True, (0,0,0))
-        screen.blit(text_surface, (0,0))
-        screen.blit(brotImage, (0,infoPanelHeight))
+        current_surface = default_font.render(f"Current point: {round(current_point[0], decimal_count)} + {round(current_point[1], decimal_count)}j", True, (0,0,0))
+
+        #Refresh background
+        info_panel_drawing_list = [(0,0)]
+        info_panel_drawing_list.append([current_range_surface])
+        info_panel_drawing_list.append([current_surface])
+        info_panel_drawing_list.append([default_font.render('Growth Factor: ', True, (0,0,0)), growth_text])
+
+        mandel_drawing_list = [(0,infoPanelHeight)]
+        mandel_drawing_list.append([brotImage])
+
+        redraw_background(screen, (255,255,255), info_panel_drawing_list, mandel_drawing_list)
 
         #The mouse is held down
         if(mouseInitialPos != None):
@@ -192,6 +195,7 @@ def main():
             pygame.draw.rect(screen, rectColor, getRect(mouseInitialPos, tempPos, graphRatio, infoPanelHeight), 2)
         # event handling, gets all event from the event queue
         for event in pygame.event.get():
+            growth_text.handle_event(event)
             #Quit
             if(event.type == pygame.QUIT):
                 running = False
@@ -207,10 +211,11 @@ def main():
                     xRange, yRange = getPoints(xRange, yRange, getRect(mouseInitialPos, tempPos, graphRatio, infoPanelHeight), graphWidth, graphHeight, infoPanelHeight)
                     mouseInitialPos = None
                     needToGenerate = True
+            #Key events
+            elif(event.type == pygame.KEYDOWN):
+                if(event.key == pygame.K_RETURN):
+                    needToGenerate = True
 
         pygame.display.flip()
 
 main()
-#generatePointsNumpy(-2, 1, -1, 1, 1000, 666, 50, 1/3, 1)
-#generatePoints(-0.106548815638, -0.103527924525, -0.890801192967, -0.889310477469, 6000, 4000, 250, 1/2)
-#generatePoints(-2, 1, -1, 1, 6000, 4000, 250, 1/3)
---
+++ NumberInputBox.py
@@ -0,0 +1,48 @@
+import pygame
+import re
+
+class NumberInputBox():
+    def __init__(self, top_left, length, height, color, font, text = ''):
+        self.top_left = top_left
+        self.length = length
+        self.height = height
+        self.color = color
+        self.text = text
+        self.is_in_focus = False
+        self.font = font
+        self.regex_to_match = r'\d{,2}(\.\d{,2})?'
+
+    #Handles the mouse and keyboard events for a game
+    def handle_event(self, event):
+        rect = pygame.Rect(self.top_left, (self.length, self.height))
+        if(event.type == pygame.MOUSEBUTTONDOWN):
+            #If the mouse was clicked within the confines of this input box
+            if(rect.collidepoint(event.pos)):
+                self.is_in_focus = True
+            else:
+                self.is_in_focus = False
+
+        elif(event.type == pygame.KEYDOWN):
+            #Only type if this is in focus
+            if(self.is_in_focus):
+                self.try_to_add_text(event.key)
+
+    #Add the key to the text
+    def try_to_add_text(self, key):
+        temp_text = (self.text + pygame.key.name(key)) if key != pygame.K_BACKSPACE else self.text[:-1]
+        print(temp_text)
+        #Make sure the new text matches the given regex. If not, do not update text
+        if(re.fullmatch(self.regex_to_match, temp_text)):
+            self.text = temp_text
+
+    #Returns the surface associated with this text box
+    def get_surface(self):
+        text_surface = pygame.Surface((self.length, self.height))
+        temp_color = (200, 200, 255) if self.is_in_focus else self.color
+        text_surface.fill(temp_color)
+        text_surface.blit(self.font.render(self.text, True, (0,0,0)), (0,0))
+        return text_surface
+
+    #Returns the number version of the text
+    def get_factor(self):
+        return float(self.text) if len(self.text) else .01
